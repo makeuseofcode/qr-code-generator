376065f47505abd96e0b5cbb6a1398ed73f5f0a9
utin-francis-peter <u.francispeter@gmail.com>
2023-07-19 12:26:18 +0100

    chore: cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,11 @@ const App = () => {
       <h1>QR Code Generator</h1>
 
       <form>
-        <input
-          required
-          type="url"
-          placeholder="Enter a valid URL"
-        />
+        <input required type="url" placeholder="Enter a valid URL" />
         <input type="submit" value="Generate" />
       </form>
 
       {/* codes to conditionally display the QR code and a download button would go here */}
-      <div className="generated-view"></div>
     </div>
   );
 };
